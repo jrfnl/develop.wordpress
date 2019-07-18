ad1977dcccb1085d459df656e548ad0655b26372
jrfnl <jrfnl@users.noreply.github.com>
2019-07-18 02:50:51 +0200

    Simplify & modernize *_Upgrader_Skin::feedback()

--- src/wp-admin/includes/class-wp-ajax-upgrader-skin.php
+++ src/wp-admin/includes/class-wp-ajax-upgrader-skin.php
@@ -113,16 +113,16 @@ class WP_Ajax_Upgrader_Skin extends Automatic_Upgrader_Skin {
 	 *
 	 * @since 4.6.0
 	 *
-	 * @param string|array|WP_Error $data Log entry data.
+	 * @param string|array|WP_Error $data    Log entry data.
+	 * @param mixed                 ...$args Optional text replacements.
 	 */
-	public function feedback( $data ) {
+	public function feedback( $data, ...$args ) {
 		if ( is_wp_error( $data ) ) {
 			foreach ( $data->get_error_codes() as $error_code ) {
 				$this->errors->add( $error_code, $data->get_error_message( $error_code ), $data->get_error_data( $error_code ) );
 			}
 		}
 
-		$args = func_get_args();
-		call_user_func_array( array( $this, 'parent::feedback' ), $args );
+		parent::feedback( $data, ...$args );
 	}
 }
